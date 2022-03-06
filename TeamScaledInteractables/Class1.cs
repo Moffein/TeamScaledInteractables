b612dbf915be6352cc01e306fb9bb61d520a2951
Moffein <22742925+Moffein@users.noreply.github.com>
2022-03-06 01:08:49 -0700

    Added config.

--- TeamScaledInteractables/Class1.cs
+++ TeamScaledInteractables/Class1.cs
@@ -7,14 +7,21 @@ using UnityEngine;
 
 namespace TeamScaledInteractables
 {
-    [BepInPlugin("com.Moffein.TeamScaledInteractables", "Team Scaled Interactables", "1.0.1")]
+    [BepInPlugin("com.Moffein.TeamScaledInteractables", "Team Scaled Interactables", "1.0.2")]
     public class TeamScaledInteractables : BaseUnityPlugin
     {
+        public static bool scaleCombat = true;
+        public static bool scaleBoss = true;
+        public static bool scaleVoid = true;
         public void Awake()
         {
             //shrineBoss = LegacyResourcesAPI.Load<SpawnCard>("spawncards/interactablespawncard/iscshrineboss");
             //shrineCombat = LegacyResourcesAPI.Load<SpawnCard>("spawncards/interactablespawncard/iscshrinecombat");
             //voidSeed = LegacyResourcesAPI.Load<SpawnCard>("spawncards/interactablespawncard/iscvoidcamp");    //Why isn't this detected?
+
+            scaleCombat = Config.Bind("Settings", "Shrine of Combat", true, "Apply scaling to Shrines of Combat.").Value;
+            scaleBoss= Config.Bind("Settings", "Shrine of the Mountain", true, "Apply scaling to Shrines of the Mountain.").Value;
+            scaleVoid = Config.Bind("Settings", "Void Seed", true, "Apply scaling to Void Seeds.").Value;
 
             IL.RoR2.SceneDirector.PopulateScene += (il) =>
             {
@@ -26,7 +33,7 @@ namespace TeamScaledInteractables
                 c.Emit(OpCodes.Ldloc_2);    //DirectorCard
                 c.EmitDelegate<Func<int, DirectorCard, int>>((cost, card) =>
                 {
-                    if (card.spawnCard.name == "iscShrineCombat" || card.spawnCard.name == "iscShrineBoss" || card.spawnCard.name == "iscVoidCamp")
+                    if ((scaleCombat && card.spawnCard.name == "iscShrineCombat") || (scaleBoss && card.spawnCard.name == "iscShrineBoss") || (scaleVoid && card.spawnCard.name == "iscVoidCamp"))
                     {
                         cost = (int)(cost * (1f + 0.5f * (Run.instance.participatingPlayerCount - 1)));
                     }
