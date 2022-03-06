d2183043585c4f2a6954602a38a0d6641e9c6f6c
Moffein <22742925+Moffein@users.noreply.github.com>
2022-03-05 21:50:16 -0700

    Add project files.

---
+++ TeamScaledInteractables/Class1.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RoR2;
+using System;
+using UnityEngine;
+
+namespace TeamScaledInteractables
+{
+    [BepInPlugin("com.Moffein.TeamScaledInteractables", "Team Scaled Interactables", "1.0.0")]
+    public class TeamScaledInteractables : BaseUnityPlugin
+    {
+        SpawnCard shrineBoss;
+        SpawnCard shrineCombat;
+        SpawnCard voidSeed;
+
+        public void Awake()
+        {
+            shrineBoss = LegacyResourcesAPI.Load<SpawnCard>("spawncards/interactablespawncard/iscshrineboss");
+            shrineCombat = LegacyResourcesAPI.Load<SpawnCard>("spawncards/interactablespawncard/iscshrinecombat");
+            voidSeed = LegacyResourcesAPI.Load<SpawnCard>("spawncards/interactablespawncard/iscvoidsuppressor");
+
+            IL.RoR2.SceneDirector.PopulateScene += (il) =>
+            {
+                ILCursor c = new ILCursor(il);
+                c.GotoNext(
+                    x => x.MatchCallvirt<DirectorCard>("get_cost")
+                    );
+                c.Index++;
+                c.Emit(OpCodes.Ldloc_2);    //DirectorCard
+                c.EmitDelegate<Func<int, DirectorCard, int>>((cost, card) =>
+                {
+                    if (card.spawnCard == shrineBoss || card.spawnCard == shrineCombat || card.spawnCard == voidSeed)
+                    {
+                        cost = (int)(cost * (1f + 0.5f * (Run.instance.participatingPlayerCount - 1)));
+                    }
+                    return cost;
+                });
+            };
+        }
+    }
+}
